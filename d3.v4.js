c40a884c0e3fc62a814b2f2c99fc6214d31038fc
arlendp <dupeng1160058696@qq.com>
2016-10-06 10:33:42 +0800

    增加d3.dispatch部分注释

--- d3.v4.js
+++ d3.v4.js
@@ -4624,7 +4624,31 @@ var   tau$1 = 2 * pi$1;
   var noop$1 = {value: function() {}};
 
   function dispatch() {
-    //将传入的参数作为键值存入Dispatch对象中
+    /*将传入的参数作为键值存入Dispatch对象中，参数以数组的形式传入并且不能有重复的元素
+     *初始化时参数只包含类型，不应包含名称，并入初始化时可以传入['click', 'drag']，在之后调用on方法时可以通过on('click.my1 drag.my2 hover', callback)这种方式来绑定回调函数
+     *在dispatch中是以如下方式存储：
+     *
+     *  {
+     *    'click': [
+     *      {
+     *        name: 'my1',
+     *        value: callback
+     *      }
+     *    ],
+     *    'drag': [
+     *      {
+     *        name: 'my2',
+     *        value: callback
+     *      }
+     *    ],
+     *    'hover': [
+     *      {
+     *        name: '',
+     *        value: callback
+     *      }
+     *    ]
+     *  }
+     */
     for (var i = 0, n = arguments.length, _ = {}, t; i < n; ++i) {
       if (!(t = arguments[i] + "") || (t in _)) throw new Error("illegal type: " + t);
       _[t] = [];
@@ -4640,7 +4664,9 @@ var   tau$1 = 2 * pi$1;
     return typenames.trim().split(/^|\s+/).map(function(t) {
       var name = "", i = t.indexOf(".");
       if (i >= 0) name = t.slice(i + 1), t = t.slice(0, i);
+      //如果绑定的事件类型在dispatch初始化时没有传入，则抛出异常
       if (t && !types.hasOwnProperty(t)) throw new Error("unknown type: " + t);
+      //没有name时默认为“”
       return {type: t, name: name};
     });
   }
@@ -4655,14 +4681,14 @@ var   tau$1 = 2 * pi$1;
           i = -1,
           n = T.length;
 
-      // If no callback was specified, return the callback of the given type and name.
+      // 如果没有callback参数，则返回以指定type和name注册的callback函数。
       if (arguments.length < 2) {
         while (++i < n) if ((t = (typename = T[i]).type) && (t = get(_[t], typename.name))) return t;
         return;
       }
 
-      // If a type was specified, set the callback for the given type and name.
-      // Otherwise, if a null callback was specified, remove callbacks of the given name.
+      // 如果传入了callback函数，则对指定的type和name设置该回调函数。
+      // 如果callback为null，则可以移除指定的回调函数
       if (callback != null && typeof callback !== "function") throw new Error("invalid callback: " + callback);
       while (++i < n) {
         if (t = (typename = T[i]).type) _[t] = set$1(_[t], typename.name, callback);
@@ -4671,14 +4697,17 @@ var   tau$1 = 2 * pi$1;
 
       return this;
     },
+    //对dispatch进行拷贝，对拷贝后的内容进行修改不会影响之前的内容
     copy: function() {
       var copy = {}, _ = this._;
       for (var t in _) copy[t] = _[t].slice();
       return new Dispatch(copy);
     },
     call: function(type, that) {
+      //第二个参数之后的参数会传入callback函数中
       if ((n = arguments.length - 2) > 0) for (var args = new Array(n), i = 0, n, t; i < n; ++i) args[i] = arguments[i + 2];
       if (!this._.hasOwnProperty(type)) throw new Error("unknown type: " + type);
+      //会调用type下的所有回调函数
       for (t = this._[type], i = 0, n = t.length; i < n; ++i) t[i].value.apply(that, args);
     },
     apply: function(type, that, args) {
@@ -4698,6 +4727,7 @@ var   tau$1 = 2 * pi$1;
   function set$1(type, name, callback) {
     for (var i = 0, n = type.length; i < n; ++i) {
       if (type[i].name === name) {
+        //如果type中已有指定的name，则将其从type数组中移除
         type[i] = noop$1, type = type.slice(0, i).concat(type.slice(i + 1));
         break;
       }
